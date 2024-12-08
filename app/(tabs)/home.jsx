3ab19ab3eceab08b82799f9575fa92eb76668b0b
Aththariq Lisan Qur'an Daulah Sentono <130519479+aththariq@users.noreply.github.com>
2024-12-09 06:52:16 +0700

    splashscreen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Bookmark = () => {
+const Home = () => {
   return (
     <View>
-      <Text>Bookmark</Text>
+      <Text>Home</Text>
     </View>
   )
 }
 
-export default Bookmark
+export default Home
